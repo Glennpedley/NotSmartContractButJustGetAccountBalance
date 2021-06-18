bbcfe4bcaa0fdfb8163334450cd791fb57804b5a
Glennpedley <80082735+Glennpedley@users.noreply.github.com>
2021-06-18 10:57:40 +0800

    Create TokenBalance.js file
    again copy from shawntabrizi

---
+++ TokenBalance.js
@@ -0,0 +1,53 @@
+window.addEventListener('load', function () {
+    if (typeof web3 !== 'undefined') {
+        console.log('Web3 Detected! ' + web3.currentProvider.constructor.name)
+        window.web3 = new Web3(web3.currentProvider);
+    } else {
+        console.log('No Web3 Detected... using HTTP Provider')
+        window.web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/<APIKEY>"));
+    }
+})
+
+const promisify = (inner) =>
+    new Promise((resolve, reject) =>
+        inner((err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        })
+    );
+
+async function getBalance() {
+    var address, wei, balance
+    address = document.getElementById("address").value;
+    wei = promisify(cb => web3.eth.getBalance(address, cb))
+    try {
+        balance = web3.fromWei(await wei, 'ether')
+        document.getElementById("output").innerHTML = balance + " ETH";
+    } catch (error) {
+        document.getElementById("output").innerHTML = error;
+    }
+}
+async function getERC20Balance() {
+    var address, contractAddress, contractABI, tokenContract, decimals, balance, name, symbol, adjustedBalance
+    address = document.getElementById("address").value
+    contractAddress = document.getElementById("contractAddress").value
+    contractABI = TokenABI
+
+    tokenContract = web3.eth.contract(contractABI).at(contractAddress)
+
+    decimals = promisify(cb => tokenContract.decimals(cb))
+    balance = promisify(cb => tokenContract.balanceOf(address, cb))
+    name = promisify(cb => tokenContract.name(cb))
+    symbol = promisify(cb => tokenContract.symbol(cb))
+
+    try {
+        adjustedBalance = await balance / Math.pow(10, await decimals)
+        document.getElementById("output2").innerHTML = adjustedBalance;
+        document.getElementById("output2").innerHTML += " " + await symbol + " (" + await name + ")";
+    } catch (error) {
+        document.getElementById("output2").innerHTML = error;
+    }
+}
